fb09d623baa9a9f7d8cd48fa0c915aebd590f99f
okkyprasetian <okkyprasetian@gmail.com>
2021-11-05 09:14:07 +0700

    fetch api for project state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useState, useEffect } from 'react'
+// import Loading from './components/Loading'
 import Header from './components/Header'
 import Cards from './components/Cards'
 import './App.css';
 
 function App() {
+
+  const [tourLs, setTourLs] = useState([])
+
+  useEffect(() => {
+    const getTourLs = async () => {
+      const response = await fetch('https://course-api.com/react-tours-project')
+      const data = await response.json()
+      return data
+    }
+
+    getTourLs().then(data => setTourLs(data))
+  }, [])
+
   return (
     <div className="App">
       <Header />
